290fdc4cd73cf45ecc39c2e0e1bf6170d142ecfc
agnuvolo <abhiji.gund@nuvolo.com>
2022-04-09 18:08:40 +0530

    changes

--- pages/api/appointment/_middleware.ts
+++ pages/api/appointment/_middleware.ts
@@ -1,35 +1,39 @@
-import type {NextRequest} from "next/server";
+import type {NextFetchEvent, NextRequest} from "next/server";
 import {NextResponse} from "next/server";
 import jwt from "jsonwebtoken";
 
-export function middleware (req: NextRequest){
-    try {
-        if (!process.env.JWT_SECRET) throw new Error("JWT_SECRET is not defined");
-        const response = NextResponse.next();
-        let token =  req["headers"].get("authorization");
+// export function middleware (req: NextRequest){
+//     try {
+//         if (!process.env.JWT_SECRET) throw new Error("JWT_SECRET is not defined");
+//         const response = NextResponse.next();
+//         let token =  req["headers"].get("authorization");
     
-        if(
-            typeof token != "undefined" &&
-            token != null
-        ){
-            const barerheader = token;
-            const baerer = barerheader.split(" ");
-            token = baerer[1];
-        }
+//         if(
+//             typeof token != "undefined" &&
+//             token != null
+//         ){
+//             const barerheader = token;
+//             const baerer = barerheader.split(" ");
+//             token = baerer[1];
+//         }
     
-        if (token){
-            const token_details = jwt.verify(token, process.env.JWT_SECRET);
+//         if (token){
+//             const token_details = jwt.verify(token, process.env.JWT_SECRET);
     
-            if (!token_details){
-                throw new Error("Authentication failed");
-            } else {
-                return response;
-            }
-        } else {
-            throw new Error("Authentication token required");
-        }
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error:any){
-        return new Response(error.message, {status: 401});
-    }
-}
+//             if (!token_details){
+//                 throw new Error("Authentication failed");
+//             } else {
+//                 return response;
+//             }
+//         } else {
+//             throw new Error("Authentication token required");
+//         }
+//     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//     } catch (error:any){
+//         return new Response(error.message, {status: 401});
+//     }
+// }
+
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+    return new Response('Hello, world!')
+  }
